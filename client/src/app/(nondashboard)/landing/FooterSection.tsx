4366e100ac279c977fbf6185513bad707db69767
ajfm88 <ale@ajfm88.com>
2025-08-19 10:16:46 -0700

    14. Update title, metadata description, favicon and links.

--- client/src/app/(nondashboard)/landing/FooterSection.tsx
+++ client/src/app/(nondashboard)/landing/FooterSection.tsx
@@ -40,36 +40,56 @@ const FooterSection = () => {
           </nav>
           <div className="flex space-x-4 mb-4">
             <a
-              href="#"
+              href="https://fb.me/ajfm88"
+              target="_blank"
+              rel="noopener noreferrer"
               aria-label="Facebook"
               className="hover:text-primary-600"
             >
               <FontAwesomeIcon icon={faFacebook} className="h-6 w-6" />
             </a>
             <a
-              href="#"
+              href="https://instagr.am/ajfm88"
+              target="_blank"
+              rel="noopener noreferrer"
               aria-label="Instagram"
               className="hover:text-primary-600"
             >
               <FontAwesomeIcon icon={faInstagram} className="h-6 w-6" />
             </a>
-            <a href="#" aria-label="Twitter" className="hover:text-primary-600">
+            <a
+              href="https://x.com/ajfm88"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Twitter"
+              className="hover:text-primary-600"
+            >
               <FontAwesomeIcon icon={faTwitter} className="h-6 w-6" />
             </a>
             <a
-              href="#"
+              href="https://linkedin.com/in/ajfm88"
+              target="_blank"
+              rel="noopener noreferrer"
               aria-label="Linkedin"
               className="hover:text-primary-600"
             >
               <FontAwesomeIcon icon={faLinkedin} className="h-6 w-6" />
             </a>
-            <a href="#" aria-label="Youtube" className="hover:text-primary-600">
+            <a
+              href="https://youtube.com/@ajfm88"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Youtube"
+              className="hover:text-primary-600"
+            >
               <FontAwesomeIcon icon={faYoutube} className="h-6 w-6" />
             </a>
           </div>
         </div>
         <div className="mt-8 text-center text-sm text-gray-500 flex justify-center space-x-4">
-          <span>© RENTiful. All rights reserved.</span>
+          <span>
+            © {new Date().getFullYear()} RENTiful. All rights reserved.
+          </span>
           <Link href="/privacy">Privacy Policy</Link>
           <Link href="/terms">Terms of Service</Link>
           <Link href="/cookies">Cookie Policy</Link>
